dc41b0243849ca83144c4ee2073f3aa08a54a39d
sehermahmud <seher.mahmud@gmail.com>
2020-06-22 00:47:07 +0600

    added PORT env

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ch403.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
